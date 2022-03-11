4907a806d2d9d75fd08c555531f1ee20a47aba0c
Adam Rumpf <arumpf@floridapoly.edu>
2022-03-10 23:20:39 -0500

    Completed output code.

--- last_attended.py
+++ last_attended.py
@@ -1,6 +1,7 @@
 """Short script for gathering statistics from an attendance CSV file."""
 
 import csv
+import datetime
 import re
 
 #==============================================================================
@@ -29,6 +30,9 @@ def report(infile, outfile=None, date=None):
         - a "Class Date" field in "[M]M/[D]D/[YY]YY" format
         - an "Attendance" field including either "present" or "absent"
     """
+
+    # Initialize reference date tuple
+    rdate = (0, 0, 0)
 
     # Initialize column indices
     namecol = -1 # student name
@@ -63,8 +67,10 @@ def report(infile, outfile=None, date=None):
             d = _date_tuple(row[datecol]) # row's date tuple
             a = _attendance_code(row[attcol]) # row's attendance code
 
-            # Add date to unique date list
+            # Add date to unique date list and update latest date
             udates.add(d)
+            if _later_date(d, rdate):
+                rdate = d
 
             # Create row entry for new students
             if n not in sdic:
@@ -76,20 +82,29 @@ def report(infile, outfile=None, date=None):
                 if _later_date(d, sdic[n][1]):
                     sdic[n][1] = d
 
+    # Choose reference date (defaults to last recorded date)
+    if date == "today":
+        dt = datetime.date.today()
+        rdate = (int(dt.year), int(dt.month), int(dt.day))
+    elif isinstance(date, str):
+        rdate = _date_tuple(date)
+
     # Initialize output string
     ostring = "Grade Report\n\nName\tRate\tDays Since Attended\n"
 
     # Generate statistics for each student
     for n in sdic:
         r = sdic[n][0]/len(udates) # attendance rate
-        g = 0 # days since last attended
+        g = _date_difference(rdate, sdic[n][1]) # days since last attended
         ostring += f'{n}\t{r:.1%}\t{g}\n'
-    ### Choose whether to print or write to file
-    ### Attendance rate
-    ### Days since last attended
 
-    ### Test code
-    print(ostring)
+    # Write to file or print to screen
+    if outfile == None:
+        print(ostring)
+    else:
+        with open(outfile, mode='w') as f:
+            f.write(ostring)
+        print("Grade report successfully written to '" + outfile + "'.")
 
 #==============================================================================
 
@@ -154,6 +169,118 @@ def _attendance_code(stratt):
         return -1
 
 #==============================================================================
+def _year_days(year):
+    """_year_days(year)
+
+    Returns the number of days in a given year.
+
+    Positional arguments:
+        year (int) - a year
+
+    Returns:
+        int - number of days in the specified year
+
+    This is computed according to the Gregorian calendar, under which a leap
+    year occurs every 4 years, unless the year is divisible by 100 and not 400.
+    """
+
+    if (year % 4 != 0) or (year % 100 == 0 and year % 400 == 0):
+        return 365
+    else:
+        return 366
+
+#==============================================================================
+def _month_days(month, year=None):
+    """_month_days(month[, year])
+
+    Returns the number of days in a given month.
+
+    Positional arguments:
+        month (int) - a month, with January = 1 and December = 12
+
+    Keyword arguments:
+        year (int) - a year (default None, which ignores leap years)
+
+    Returns:
+        int - number of days in the specified month of the specified year, or
+            -1 if given an invalid month
+    """
+
+    # Categorize depending on month
+    if month in {4, 6, 9, 11}:
+        return 30
+    elif month in {1, 3, 5, 7, 8, 10, 12}:
+        return 31
+    elif month == 2:
+        if year == None:
+            return 28
+        else:
+            return 28 + (_year_days(year) - 365)
+    else:
+        return -1
+
+#==============================================================================
+def _increment_date(date):
+    """_increment_date(date)
+
+    Increments a given date by one day.
+
+    Positional arguments:
+        date (tuple) - date tuple
+
+    Returns:
+        tuple - date tuple one day later than the given date
+    """
+
+    # Add 1 to the day
+    d = (date[0], date[1], date[2] + 1)
+
+    # Carry over the month if needed
+    if d[2] > _month_days(d[1], year=d[0]):
+        d = (d[0], d[1] + 1, 1)
+        
+        # Carry over the year if needed
+        if d[1] > 12:
+            d = (d[0] + 1, 1, 1)
+
+    return d
+
+#==============================================================================
+def _date_difference(date1, date2):
+    """_date_difference(date1, date2)
+
+    Computes date1 minus date2, as a number of days.
+
+    Positional arguments:
+        date1 (tuple) - first date tuple
+        date2 (tuple) - second date tuple
+
+    Returns:
+        int - number of days that need to be added to date2 to get to date1
+    """
+
+    # Determine the sign of the result
+    negate = False
+    d1 = date1
+    d2 = date2
+    if not _later_date(date1, date2):
+        negate = True
+        d2 = date1
+        d1 = date2
+
+    # Count days from earlier date to later date
+    days = 0
+    while _later_date(d1, d2):
+        days += 1
+        d2 = _increment_date(d2)
+
+    # Return number of days counted, negated properly
+    if negate:
+        return -days
+    else:
+        return days
+
+#==============================================================================
 
 def _later_date(date1, date2):
     """_later_date(date1, date2)
@@ -185,6 +312,3 @@ def _later_date(date1, date2):
                 return True
             else:
                 return False
-    
-### Test code
-report("mac2312-02.csv")
